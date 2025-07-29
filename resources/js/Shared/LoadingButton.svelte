ed4ed181779c37f58ebd3beb00231d00740f408a
Kresna Satya <halo@kresna.me>
2025-07-29 15:05:24 +0800

    fix: use $props run in LoadingButton component

--- resources/js/Shared/LoadingButton.svelte
+++ resources/js/Shared/LoadingButton.svelte
@@ -1,22 +1,11 @@
-<!-- @migration-task Error while migrating Svelte code: migrating this component would require adding a `$props` rune but there's already a variable named props.
-     Rename the variable and try again or migrate by hand. -->
-<!-- @migration-task Error while migrating Svelte code: migrating this component would require adding a `$props` rune but there's already a variable named props.
-     Rename the variable and try again or migrate by hand. -->
-<!-- @migration-task Error while migrating Svelte code: migrating this component would require adding a `$props` rune but there's already a variable named props.
-     Rename the variable and try again or migrate by hand. -->
 <script>
-  export let loading = false
-
-  $: props = {
-    ...$$restProps,
-    class: `flex items-center ${$$restProps.class || ''}`,
-  }
+  let { children, loading = $bindable(false), class: className, ...props } = $props()
 </script>
 
-<button {...props} disabled={loading}>
+<button class="flex items-center {className}" {...props} disabled={loading}>
   {#if loading}
     <div class="btn-spinner mr-2"></div>
   {/if}
 
-  <slot />
+  {@render children()}
 </button>
