49b4fd33825b7120b5bfc178115b9241b3218e7e
Kresna Satya <kresnasatya72@gmail.com>
2025-07-29 21:04:53 +0800

    feat: add server side rendering script

---
+++ resources/js/ssr.js
@@ -0,0 +1,14 @@
+import { createInertiaApp } from '@inertiajs/svelte'
+import createServer from '@inertiajs/svelte/server'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { render } from 'svelte/server'
+
+createServer((page) => {
+    createInertiaApp({
+        page,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.svelte`, import.meta.glob('./Pages/**/*.svelte')),
+        setup({ App, props }) {
+            return render(App, { props });
+        }
+    })
+})
